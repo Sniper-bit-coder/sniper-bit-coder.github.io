1a86ca9ae9e2eb9d146f9a03e5bf8fe5a7be0996
sniper-bit-coder <19951464362@163.com>
2025-10-04 15:28:19 +0800

    Deploy initial website version

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1759559154',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/categories/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
